d381ec6259ea2915d51d3fa78ab03a58d3238b9e
claudia v <claudia4ro@yahoo.com>
2020-07-22 13:12:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
    end
   
-  def login
+  def create
     if params[:name].nil? || params[:name].empty?
       redirect_to '/login'
     else 
